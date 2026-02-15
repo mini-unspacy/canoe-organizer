e5417b4b4159ddd60b13865045624a3ffb3f67e7
mini li <ken.c.li.alert@gmail.com>
2026-02-15 03:35:19 -0800

    Revert "Prevent staging area paddlers from shifting during drag"
    This reverts commit 244eb9245c145235a58eb12e9157acf01e3db2aa.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2288,7 +2288,7 @@ function AppMain({ currentUser, onLogout }: { currentUser: User; onLogout: () =>
                         }) : (
                           <span className="text-sm w-full text-center mt-4" style={{ color: '#c0c0c0' }}>drag paddlers here to unassign</span>
                         )}
-                        <div style={{ display: 'none' }}>{provided.placeholder}</div>
+                        {provided.placeholder}
                       </div>
                     );
                   }}
